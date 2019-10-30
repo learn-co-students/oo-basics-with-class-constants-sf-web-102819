1514bcf412ebd5f6cac9c23d3f38c8206aaa4e50
Will Diep <widiep@gmail.com>
2019-10-30 15:22:23 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,26 @@
+# require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << @brand
+    end
+
+    # BRANDS << @brand if BRANDS.include?(brand) == false
+    
+    # BRANDS << @brand if !BRANDS.include?(brand)
+
   end
+
+  # def brand=(new_brand)
+    BRANDS << @brand
+  # end
 
   def cobble
     self.condition = "new"
@@ -12,3 +28,14 @@ class Shoe
   end
 
 end
+
+uggs = Shoe.new("Uggs")
+rainbow = Shoe.new("Rainbow")
+nike1 = Shoe.new("Nike")
+nike2 = Shoe.new("Nike")
+
+p Shoe::BRANDS
+
+
+# binding.pry
+# puts "Shoe"
